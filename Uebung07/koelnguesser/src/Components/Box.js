c3b3263ae7f2153759b93f4f8f891e42e51b309e
Billamw <66533754+Billamw@users.noreply.github.com>
2023-12-13 13:13:44 +0100

    update

---
+++ Uebung07/koelnguesser/src/Components/Box.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Box.css'
+
+const Box = () => {
+  return (
+    <div class="container">
+        <div class="box">
+            Box
+        </div>
+    </div>
+  )
+}
+
+export default Box
---
+++ Uebung07/koelnguesser/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div class="header">Header</div>
+  )
+}
+
+export default Header
