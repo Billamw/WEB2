aab378ab37233e944a7b31ad8de55a79b225277b
Billamw <66533754+Billamw@users.noreply.github.com>
2023-12-20 13:17:19 +0100

    update

--- Uebung07/koelnguesser/src/App.js
+++ Uebung07/koelnguesser/src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Button from "./Components/Button.js";
 import Header from "./Components/Header.js";
 import Box from "./Components/Box.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Homepage from "./Components/Homepage.js";
 import "./App.css";
 
 const App = () => {
-  return (
     <div>
-      <Header></Header>
-      <Box></Box>
-    </div>
-  );
+        <Header></Header>
+        <Box></Box>
+    </div>;
 };
 
 export default App;
---
+++ Uebung07/koelnguesser/src/Components/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./Header.js";
+import Box from "./Box.js";
+
+const Homepage = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Box></Box>
+        </div>
+    );
+};
+
+export default Homepage;
