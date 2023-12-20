1be6275b0060b8422c54a4a930f488efa369b455
Billamw <66533754+Billamw@users.noreply.github.com>
2023-12-20 15:17:47 +0100

    Update UseHighscore.js

--- Uebung07/koelnguesser/src/Components/UseHighscore.js
+++ Uebung07/koelnguesser/src/Components/UseHighscore.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
 
+// https://www.robinwieruch.de/local-storage-react/
+// const person = { firstName: "Robin", lastName: "Wieruch" };
+
+// localStorage.setItem("user", JSON.stringify(person));
+
+// const stringifiedPerson = localStorage.getItem("user");
+// const personAsObjectAgain = JSON.parse(stringifiedPerson);
+
 const UseHighscore = () => {
     const [highscore, setHighscore] = useState([]);
 
