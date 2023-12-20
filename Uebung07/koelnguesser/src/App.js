104ddc37250029a4e020aca56bf73ada2adfb992
Billamw <66533754+Billamw@users.noreply.github.com>
2023-12-20 13:26:55 +0100

    Update App.js

--- Uebung07/koelnguesser/src/App.js
+++ Uebung07/koelnguesser/src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Header from "./Components/Header.js";
 import Box from "./Components/Box.js";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./Components/Homepage.js";
 import "./App.css";
 
 const App = () => {
-    <div>
-        <Router>
-            <Route exact path="/" component={Homepage} />
-        </Router>
-        ;
-    </div>;
+    <Router>
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+        </Routes>
+    </Router>;
 };
 
 export default App;
