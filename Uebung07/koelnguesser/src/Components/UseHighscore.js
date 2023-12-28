f05f54d57a89afcf2fcec5b58942f0074a796286
Billamw <66533754+Billamw@users.noreply.github.com>
2023-12-28 22:35:24 +0100

    Update UseHighscore.js

--- Uebung07/koelnguesser/src/Components/UseHighscore.js
+++ Uebung07/koelnguesser/src/Components/UseHighscore.js
@@ -1,15 +1,39 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-// https://www.robinwieruch.de/local-storage-react/
-// const person = { firstName: "Robin", lastName: "Wieruch" };
+// // https://www.robinwieruch.de/local-storage-react/
+// // const person = { firstName: "Robin", lastName: "Wieruch" };
 
-// localStorage.setItem("user", JSON.stringify(person));
+// // localStorage.setItem("user", JSON.stringify(person));
 
-// const stringifiedPerson = localStorage.getItem("user");
-// const personAsObjectAgain = JSON.parse(stringifiedPerson);
+// // const stringifiedPerson = localStorage.getItem("user");
+// // const personAsObjectAgain = JSON.parse(stringifiedPerson);
+
+// const UseHighscore = () => {
+//     const [highscore, setHighscore] = useState([]);
+
+//     const addHighscore = (name, score) => {
+//         setHighscore([...highscore, { name, score }]);
+//     };
+
+//     return {
+//         highscore,
+//         addHighscore,
+//     };
+// };
+
+// export default UseHighscore;
+
+import { useState, useEffect } from "react";
 
 const UseHighscore = () => {
-    const [highscore, setHighscore] = useState([]);
+    const [highscore, setHighscore] = useState(() => {
+        const savedHighscores = localStorage.getItem("highscores");
+        return savedHighscores ? JSON.parse(savedHighscores) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("highscores", JSON.stringify(highscore));
+    }, [highscore]);
 
     const addHighscore = (name, score) => {
         setHighscore([...highscore, { name, score }]);
