d8136475b20719e6e42efda009f6398ae1c0d184
Billamw <66533754+Billamw@users.noreply.github.com>
2024-01-08 21:49:27 +0100

    Update index.html and App.css files

--- Uebung07/koelnguesser/src/Components/Cataas.js
+++ Uebung07/koelnguesser/src/Components/Cataas.js
@@ -1,13 +1,48 @@
-import React from "react";
-import Box from "./Box";
+import React, { useEffect, useState } from "react";
+import Box from "./Box.js";
 
 const Cataas = () => {
+    const [catData, setCatData] = useState(null);
+    const [showJson, setShowJson] = useState(false);
+
+    const fetchCat = () => {
+        fetch("https://cataas.com/cat?json=true")
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.size > 40000) {
+                    fetchCat();
+                } else {
+                    setCatData(data);
+                }
+            })
+            .catch((error) => console.error(error));
+    };
+
+    useEffect(() => {
+        fetchCat();
+    }, []);
+
     return (
-        <div>
-            <Box
-                component={<img src="https://cataas.com/cat" alt="cat" />}
-            ></Box>
-        </div>
+        <Box
+            component={
+                <div className="cataasContainer">
+                    <div className="imageContainer">
+                        <img
+                            src={`https://cataas.com/cat/${catData?._id}`}
+                            alt="cat"
+                        />
+                        <button onClick={fetchCat}>Load new cat</button>
+                        <i
+                            className="material-icons"
+                            onClick={() => setShowJson(!showJson)}
+                        >
+                            {showJson ? "toggle_on" : "toggle_off"}
+                        </i>
+                    </div>
+                    {showJson && <pre>{JSON.stringify(catData, null, 2)}</pre>}
+                </div>
+            }
+        ></Box>
     );
 };
 
